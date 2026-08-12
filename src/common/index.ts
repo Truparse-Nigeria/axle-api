edc13705b1c104575a47062bfe68a2e7c31b3d14
Okonkwo Henry <henryokonkwo01@gmail.com>
2026-08-12 16:10:16 +0100

    update

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,6 +1,6 @@
 export * from "./enum";
+export * from "./constant";
 export * from "./interface";
 export * from "./config";
 export * from "./utils";
 export * from "./validator";
-export * from "./constant";
