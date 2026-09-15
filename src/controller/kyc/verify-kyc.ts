d9a5dfa89675ef0d3c5726da973c883f50e2f233
Okonkwo Henry <henryokonkwo01@gmail.com>
2026-09-15 18:56:46 +0100

    update

--- src/controller/kyc/verify-kyc.ts
+++ src/controller/kyc/verify-kyc.ts
@@ -112,6 +112,8 @@ export const verifyKyc = catchAsync(async (req, res) => {
 
   const response = await dojahVerification(reference);
 
+  console.log(response)
+
   if (!response.data || response.error) {
     throw new AppError("KYC Verification failed. Try again");
   }
