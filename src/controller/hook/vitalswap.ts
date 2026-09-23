2b8d9340279d308ba2e857f1ff156bbdc95ef508
Okonkwo Henry <henryokonkwo01@gmail.com>
2026-09-23 12:07:33 +0100

    Update

--- src/controller/hook/vitalswap.ts
+++ src/controller/hook/vitalswap.ts
@@ -1,7 +1,18 @@
+import { AppError } from "@/common";
 import { catchAsync } from "@/middleware";
 import { sanitizeFilter } from "mongoose";
 
 export const vitalSwapHook = catchAsync(async (req, res) => {
   const payload = sanitizeFilter(req.body);
-});
 
+  const virtualBankAccount = (payload: any) => {
+    
+  };
+
+  switch (payload.event_name) {
+    case "created_virtual_bank_account":
+      return virtualBankAccount(payload);
+    default:
+      throw new AppError("Invalid event type", 400);
+  }
+});
