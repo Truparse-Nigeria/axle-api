892f6afc075fab1f61a7e877eb07d28c29fd9415
Okonkwo Henry <henryokonkwo01@gmail.com>
2026-09-24 10:44:20 +0100

    update

--- src/controller/multicurrency/create-fiat-account.ts
+++ src/controller/multicurrency/create-fiat-account.ts
@@ -69,6 +69,8 @@ export const createFiatAccount = catchAsync(async (req, res) => {
 
     const walletId = user.identifier.vitalswap.wallets?.[currency];
 
+    console.log("walletId", walletId);
+
     if (!walletId) {
       const fullUser = await setVitalSwapWalletId(
         user.identifier.vitalswap.user,
